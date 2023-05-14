343de7bb8fbabc81cc10e799331f10e821b5ae98
yoruwitch <yoru.witch4@gmail.com>
2023-05-13 21:40:14 -0300

    Feat: project finished

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# staticdex
+# StaticDex
+
+-   Projeto criado durante a Semana do 0 ao Programador Contrato oferecido pelos irmãos gêmeos do [Dev em dobro](https://devemdobro.com/).
+-   Possui algumas alterções quanto à estilização, mas segue quase o mesmo padrão. Projeto feito para aprendizado de novas estilizações usando HTML, CSS e JS.
---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const button = document.querySelector("#botao-alterar-tema");
+const body = document.querySelector("body");
+const imgChangeTheme = document.querySelector(".imagem-botao");
+
+button.addEventListener("click", () => {
+    const darkModeOn = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+
+    if (darkModeOn) {
+        imgChangeTheme.setAttribute("src", "./imgs/sun.png");
+    } else {
+        imgChangeTheme.setAttribute("src", "./imgs/moon.png");
+    }
+});
